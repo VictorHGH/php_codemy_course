67e6530375c4dd663662fb7009e29899a7b9924a
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-30 12:51:54 -0600

    [New] Associative Arrays and lopp through them

--- index.php
+++ index.php
@@ -9,48 +9,19 @@
 </head>
 
 <body bgcolor="#f3f3f3">
-	<div style="display: grid; place-items: center;">
+	<div style="display: grid; place-items: center; margin-top: 30px;">
 		<?php
-		// Arrays and loop through them
-		$name = "Jeff";
-		$friends = ["Victor", "Juan", "Pedro", "Maria", "Ana", $name];
+		// Associative Arrays and loop through them
+		$fav_color = [
+			"Victor" => "Green",
+			"Juan" => "Blue",
+			"Pedro" => "Red",
+			"Maria" => "Yellow",
+			"Ana" => "Pink"
+		];
 
-		############
-		# for loop #
-		############
-		echo "<br>";
-		for ($i = 0; $i < count($friends); $i++) {
-			echo $i + 1 . ". $friends[$i]<br>";
-		}
-
-		################
-		# Foreach loop #
-		################
-		echo "<br>";
-		$i = 1;
-		foreach ($friends as $friend) {
-			echo $i . ". $friend<br>";
-			$i++;
-		}
-
-		##############
-		# While loop #
-		##############
-		echo "<br>";
-		$i = 0;
-		while ($i < count($friends)) {
-			echo $i + 1 . ". $friends[$i]<br>";
-			$i++;
-		}
-
-		################
-		# Reverse loop #
-		################
-		echo "<br>";
-		$i = count($friends) - 1;
-		while ($i >= 0) {
-			echo $i + 1 . ". $friends[$i]<br>";
-			$i--;
+		foreach ($fav_color as $key => $value) {
+			echo "The favorite color of $key is $value <br>";
 		}
 		?>
 
