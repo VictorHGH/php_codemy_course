1fa2dee291000742b39d97280c864f37909d8c06
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-08-30 12:34:49 -0600

    [New] Arrays and loop through them

--- index.php
+++ index.php
@@ -11,9 +11,46 @@
 <body bgcolor="#f3f3f3">
 	<div style="display: grid; place-items: center;">
 		<?php
-		// For loop
-		for ($num = 0; $num <= 10; $num++) {
-			echo "Your num is: $num<br/>";
+		// Arrays and loop through them
+		$name = "Jeff";
+		$friends = ["Victor", "Juan", "Pedro", "Maria", "Ana", $name];
+
+		############
+		# for loop #
+		############
+		echo "<br>";
+		for ($i = 0; $i < count($friends); $i++) {
+			echo $i + 1 . ". $friends[$i]<br>";
+		}
+
+		################
+		# Foreach loop #
+		################
+		echo "<br>";
+		$i = 1;
+		foreach ($friends as $friend) {
+			echo $i . ". $friend<br>";
+			$i++;
+		}
+
+		##############
+		# While loop #
+		##############
+		echo "<br>";
+		$i = 0;
+		while ($i < count($friends)) {
+			echo $i + 1 . ". $friends[$i]<br>";
+			$i++;
+		}
+
+		################
+		# Reverse loop #
+		################
+		echo "<br>";
+		$i = count($friends) - 1;
+		while ($i >= 0) {
+			echo $i + 1 . ". $friends[$i]<br>";
+			$i--;
 		}
 		?>
 
