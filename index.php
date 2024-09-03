68f5d0c7ba0dd3aea23a1041ebe24515b39584b6
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-09-02 22:35:38 -0600

    [New] bootstrap and navbar

--- index.php
+++ index.php
@@ -6,22 +6,56 @@ include("variables.php")
 
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Codemy_course</title>
+	<title>Bootstrap demo</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body bgcolor="#f3f3f3">
-	<?php require_once("navbar.php") ?>
-	<div style="display: grid; place-items: center; margin-top: 30px;">
-		<?php
-			echo $site_title
-		?>
+<body>
+	<div class="container">
+		<?php require_once("navbar.php") ?>
+		<h1>Hello, world!</h1>
+
+		<form class="myForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+			Name: <input type="text" name="name" />
+			<br />
+			<br />
+			<input type="submit" class="btn btn-secondary">
+		</form>
+
+		<p>Copyright (c) <?php echo $my_name; ?> - All Rights Reserved</p>
 	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
+<style>
+	* {
+		font-family: Arial, sans-serif;
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	h1 {
+		margin-top: 20px;
+		text-align: center;
+	}
+
+	.myForm {
+		margin-top: 40px;
+		text-align: center;
+	}
+
+	p {
+		margin-top: 100px;
+		text-align: center;
+		font-size: 0.8rem;
+	}
+</style>
 
 </html>
